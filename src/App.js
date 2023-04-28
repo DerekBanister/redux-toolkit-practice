a065ee72d9b65efa3f29fd61c6c5f02812276049
unknown <ptownderek@yahoo.com>
2023-04-28 09:58:51 -0700

    dispatching actions now, dom reflecting current state when userslist changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import './App.css';
 // Use selector is used to read state of store
-import { useSelector } from 'react-redux';
-
-//grabs current state of users list
+import { useSelector, useDispatch } from 'react-redux';
+import { addUser } from './features/userReducer';
+import { useState } from 'react';
 
 
 function App() {
+  // useDispatch is used to dispatch actions.
+  const dispatch = useDispatch();
   const userList = useSelector((state) => state.users.value);
+
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+
+
   return (
     <div className="App">
       {""}
       <div className="addUser">
-        <input type="text" placeholder='Name...'></input>
-        <input type="text" placeholder='Username...'></input>
-        <button>Add User</button>
+        <input
+          type="text"
+          placeholder='Name...'
+          onChange={(event) => {
+            setName(event.target.value)
+          }}
+        ></input>
+        <input
+          type="text"
+          placeholder='Username...'
+          onChange={(event) => {
+            setUsername(event.target.value)
+          }}
+        ></input>
+        <button onClick={() => {
+          dispatch(addUser({
+            id: Math.floor(Math.random() * 1000),
+            name: name,
+            username: username
+          }))
+        }}>Add User</button>
 
         <div className="displayUsers"></div>
         {userList.map((user) => {
@@ -21,6 +46,7 @@ function App() {
             <div className="user" key={user.id}>
               <h3>{user.name}</h3>
               <h3>{user.username}</h3>
+              <br></br>
             </div>
           );
         })}
--- src/features/userReducer.js
+++ src/features/userReducer.js
@@ -9,9 +9,14 @@ export const userSlice = createSlice({
     initialState: { value: data },
     reducers: {
         addUser: (state, action) => {
-            // code for adding a user
+            // code for adding a user  
+            state.value.push(action.payload)
         }
     }
 })
 
+// Action creators are generated for each case reducer function
+export const { addUser } = userSlice.actions;
+
+// Can access value of state by using useSelector
 export default userSlice.reducer;
