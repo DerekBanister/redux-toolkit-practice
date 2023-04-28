7c0659e48c195cbf63d9745aa491491b59d2a2ed
unknown <ptownderek@yahoo.com>
2023-04-28 07:10:29 -0700

    seed data added in data.js and added to my reducer

--- src/features/userReducer.js
+++ src/features/userReducer.js
@@ -1,6 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
-
+import { data } from '../data.json'
 export const userSlice = createSlice({
     name: "users",
-    initialState: { value: [] },
+    initialState: { value: data },
+    reducers: {
+        addUser: (state, action) => {
+            // code for adding a user
+        }
+    }
 })
