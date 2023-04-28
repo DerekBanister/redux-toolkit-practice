fb3c0498b449e380624ca6bf55e134f41906880c
unknown <ptownderek@yahoo.com>
2023-04-28 10:30:03 -0700

    added some light styling and cleaned up component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,51 @@
 import './App.css';
-// Use selector is used to read state of store
 import { useSelector, useDispatch } from 'react-redux';
 import { addUser } from './features/userReducer';
 import { useState } from 'react';
 
-
 function App() {
-  // useDispatch is used to dispatch actions.
   const dispatch = useDispatch();
   const userList = useSelector((state) => state.users.value);
 
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
 
-
   return (
     <div className="App">
-      {""}
       <div className="addUser">
         <input
           type="text"
           placeholder='Name...'
-          onChange={(event) => {
-            setName(event.target.value)
-          }}
-        ></input>
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
         <input
           type="text"
           placeholder='Username...'
-          onChange={(event) => {
-            setUsername(event.target.value)
-          }}
-        ></input>
-        {/* using shorthand notation for the dispatch */}
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
         <button onClick={() => {
           dispatch(addUser({
-            // allows us to get the last id in the array and add 1 to it
             id: userList[userList.length - 1].id + 1,
             name,
             username
-          }))
+          }));
+          setName('');
+          setUsername('');
         }}>Add User</button>
-
-        <div className="displayUsers"></div>
+      </div>
+      <div className="displayUsers">
         {userList.map((user) => {
           return (
             <div className="user" key={user.id}>
               <h3>{user.name}</h3>
               <h3>{user.username}</h3>
-              <br></br>
             </div>
           );
         })}
       </div>
-    </div >
+    </div>
   );
 }
 
